86c3a9700fdbbcb408e6714f941de4c7cbf8c1e3
Tibor Simon <tibor@tiborsimon.io>
2015-09-08 08:48:38 +0200

    Modified initialization script

--- activate.bat
+++ activate.bat
@@ -1,2 +1,2 @@
-@echo off\
+@echo off
 call site-env/Scripts/activate.bat
--- init-environment.bat
+++ init-environment.bat
@@ -1,7 +1,29 @@
+@echo off
+@echo.
+@echo "Creating virtual env in folder site-env.."
 virtualenv site-env
+
+@echo.
+@echo "Activating virtual environment.."
 call activate
+
+@echo.
+@echo "Installing pip-tools.."
 pip install pip-tools
+
+@echo.
+@echo "Removing old requirements.txt file.."
 rm -f requirements.txt
+
+@echo.
+@echo "Compile requirements.."
 pip-compile
+
+@echo.
+@echo "Syncing requirements.."
 pip-sync
+
+@echo.
+@echo "Finished! Listing installed packages.."
+pip list
 pause
