3c5a4cf8f47771c6e4101cec069cf1169ed8acb2
Tibor Simon <tibor@tiborsimon.io>
2015-09-17 22:35:01 +0200

    Finalized sidebar fading

--- themes/base-bootstrap/static/js/custom.js
+++ themes/base-bootstrap/static/js/custom.js
@@ -9,6 +9,29 @@ $(function () {
 
 
  // ----------------------------------------------------------------------------
+// Helper function to find out state
+function findBootstrapEnvironment() {
+    var envs = ["xs", "sm", "md", "lg"],    
+        doc = window.document,
+        temp = doc.createElement("div");
+
+    doc.body.appendChild(temp);
+
+    for (var i = envs.length - 1; i >= 0; i--) {
+        var env = envs[i];
+
+        temp.className = "hidden-" + env;
+
+        if (temp.offsetParent === null) {
+            doc.body.removeChild(temp);
+            return env;
+        }
+    }
+    return "";
+}
+
+
+ // ----------------------------------------------------------------------------
 // Redirect external links to new tab
 $('a').each(function() {
    var a = new RegExp('/' + window.location.host + '/');
@@ -24,34 +47,57 @@ $('a').each(function() {
 
  // ----------------------------------------------------------------------------
 // Irrelevant fading
+var cookie_name = 'sidebar-fade'
 var cookie_expires = 365;
-$(function () {
-  if ($.cookie('irrelevant-off')) {
-    $("#irrelevant-on-btn").prop('checked', false);
-    $("#irrelevant-off-btn").prop('checked', true);
-    $("#irrelevant-on-btn").parent().removeClass('active');
-    $("#irrelevant-off-btn").parent().addClass('active');
-    $(".irrelevant").css("opacity", "1");
-    $(".irrelevant").removeClass("irrelevant-content");
-  };
-})
 
-$(".irrelevant-content").mouseenter(function(){
-    $(".irrelevant-content").css("opacity", "1");
+function turn_on_fading() {
+  $("#irrelevant-on-btn").prop('checked', true);
+  $("#irrelevant-off-btn").prop('checked', false);
+  $("#irrelevant-on-btn").parent().addClass('active');
+  $("#irrelevant-off-btn").parent().removeClass('active');
+
+  $(".irrelevant").addClass("irrelevant-fade");
+}
+
+function turn_off_fading() {
+  $("#irrelevant-on-btn").prop('checked', false);
+  $("#irrelevant-off-btn").prop('checked', true);
+  $("#irrelevant-on-btn").parent().removeClass('active');
+  $("#irrelevant-off-btn").parent().addClass('active');
+  
+  $(".irrelevant").removeClass("irrelevant-fade");
+}
+
+function process_fading() {
+  var env = findBootstrapEnvironment()
+  if (env == "xs" || env == "sm") {
+    $("#irrelevant-toggle").css("display", "none");
+    turn_off_fading();
+  } else {
+    $("#irrelevant-toggle").css("display", "block");
+    if ($.cookie(cookie_name)) {
+      turn_on_fading();
+    } else {
+      turn_off_fading();
+    }
+  }  
+}
+
+$(function () {
+  process_fading();
 });
-$(".irrelevant-content").mouseleave(function(){
-    $(".irrelevant-content").css("opacity", "0.1");
-});
+
+window.addEventListener('resize', process_fading);
 
 $("#irrelevant-on-btn").change(function () {
-  $.removeCookie('irrelevant-off', { expires: cookie_expires, path: '/' });
-  $(".irrelevant").css("opacity", "0.1");
-  $(".irrelevant").addClass("irrelevant-content");
+  if (confirm('By turning on this feature you allow tiborsimon.io to place a tiny cookie on your machine to remember this setting.')) { 
+    $.cookie(cookie_name, 1, { expires: cookie_expires, path: '/' });
+    process_fading();
+  }
 });
 
 $("#irrelevant-off-btn").change(function () {
-  $.cookie('irrelevant-off', '1', { expires: cookie_expires, path: '/' });
-  $(".irrelevant-content").css("opacity", "1");
-  $(".irrelevant").removeClass("irrelevant-content");
+  $.removeCookie(cookie_name, { expires: cookie_expires, path: '/' });
+  process_fading();
 });
 
