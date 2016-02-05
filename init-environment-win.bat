c2f45f54529c1447aec03f477f8921e5fc9194ce
Tibor Simon <tibor@tiborsimon.io>
2016-02-05 22:50:46 +0100

    pip-tools removed and pip-npm confloct solved

--- init-environment-win.bat
+++ init-environment-win.bat
@@ -11,22 +11,7 @@ call activate
 @echo.
 @echo -------------------------------------------------------------------------
 @echo I N S T A L L I N G   P I P - T O O L S
-pip install pip-tools
-
-@echo.
-@echo -------------------------------------------------------------------------
-@echo R E M O V I N G   O L D   R E Q U I R E M E N T S . T X T   F I L E
-rm -f requirements.txt
-
-@echo.
-@echo -------------------------------------------------------------------------
-@echo C O M P I L E   R E Q U I R E M E N T S
-pip-compile
-
-@echo.
-@echo -------------------------------------------------------------------------
-@echo S Y N C I N G   R E Q U I R E M E N T S
-pip-sync
+pip install -r requirements-pip.txt
 
 @echo.
 @echo -------------------------------------------------------------------------
