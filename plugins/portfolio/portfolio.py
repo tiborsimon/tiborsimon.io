25240e91933aa710e27e811eef1050f6b6fa391d
Tibor Simon <tibor@tiborsimon.io>
2016-02-13 00:20:43 +0100

    Portfolio, about section

--- plugins/portfolio/portfolio.py
+++ plugins/portfolio/portfolio.py
@@ -4,33 +4,10 @@ from bs4 import BeautifulSoup
 import json
 from pprint import pprint
 from dateutil.parser import parse
+import random
+
 
 PELICAN = None
-
-
-PORTFOLIO_TEMPLATE = '''
-<section id="portfolio" class="section section-portfolio">
-  <div class="animate-up">
-    <h2 class="section-title">Portfolio</h2>
-
-    <div class="filter">
-      <div class="filter-inner">
-        <div class="filter-btn-group">
-          <button data-filter="*">All</button>
-          {navigation}
-        </div>
-        <div class="filter-bar">
-          <span class="filter-bar-line"></span>
-        </div>
-      </div>
-    </div>
-
-    <div class="grid">
-      <div class="grid-sizer"></div>
-      {grid}
-    </div>
-  </div>
-</section>'''
 
 
 def load_portfolios():
@@ -48,154 +25,55 @@ def load_portfolios():
   return portfolios
 
 
-def render_categories(portfolios):
-  categories = []
-  for portfolio in portfolios:
-    if isinstance(portfolio['category'], list):
-      for c in portfolio['category']:
-        categories.append(c)
-    else:
-      categories.append(portfolio['category'])
-  categories = sorted(set(categories))
-  ret = ''
-  for c in categories:
-    ret += '<button data-filter=".{0}">{1}</button>\n'.format(c, c.capitalize())
-  return ret
-
-
-EMBED_IMG = '''
-<img class="inline-img" src="{image_src}" alt="{image_alt}"/>'''
-
-EMBED_VIDEO = '''
-<div class="inline-embed">
-  {embed_code}
-</div>'''
-
-def render_box_embed(box):
-  if 'image' in box.keys():
-    box_embed = EMBED_IMG.format(image_src=PELICAN.settings['SITEURL'] + box['image']['url'], image_alt=box['image']['alt'])
-  elif 'embed' in box.keys():
-    box_embed = EMBED_VIDEO.format(embed_code=box['embed'])
-  else:
-    print('Invalid slide type! Aborting..')
-    return None
-  return box_embed
-
-
-INLINE_BOX = '''
-<div id="portfolio{portfolio_number}-inline{inline_id}" class="fancybox-inline-box">
-  {embed}
-  <div class="inline-cont">
-    <div class="inline-text">
-      <h2 class="inline-title">{title}</h2>
-      <p>{description}</p>
-    </div>
-  </div>
-</div>'''
-
-INLINE_BOX_LINK = '''
-<a class="portfolioFancybox hidden" data-fancybox-group="portfolioFancybox{portfolio_number}" href="#portfolio{portfolio_number}-inline{inline_id}"></a>'''
-
-def render_inline_boxes(portfolios, portfolio):
-  inline_boxes = ''
-  inline_box_links = ''
-  offset = False
-  for box in portfolio['slides']:
-    box_embed = render_box_embed(box)
-
-    inline_box = INLINE_BOX.format(
-      embed=box_embed,
-      title=box['title'],
-      description=box['content'],
-      portfolio_number=portfolios.index(portfolio),
-      inline_id=portfolio['slides'].index(box)
-    )
-
-    inline_box_link = ''
-
-    if offset:
-      inline_box_link = INLINE_BOX_LINK.format(
-        portfolio_number=portfolios.index(portfolio),
-        inline_id=portfolio['slides'].index(box)
-      )
-
-    inline_boxes += inline_box
-    inline_box_links += inline_box_link
-
-    offset = True
-
-  return (inline_boxes, inline_box_links)
-
-
 PORTFOLIO_ITEM = '''
-<div class="grid-item {size} {category}">
-  <div class="grid-box">
-    <figure class="portfolio-figure">
-      <img src="{thumbnail_src}" alt="{thumbnail_alt}"/>
-      <figcaption class="portfolio-caption">
-        <div class="portfolio-caption-inner">
-          <h3 class="portfolio-title">{title}</h3>
-          <h4 class="portfolio-cat">{category_display} {date}</h4>
-
-          <div class="btn-group">
-            <a class="btn-link" href="{button_url}" target="_blank"><i class="icon icon-link"></i></a>
-            <a class="portfolioFancybox btn-zoom" data-fancybox-group="portfolioFancybox{portfolio_number}" href="#portfolio{portfolio_number}-inline0"><i class="icon icon-eye"></i></a>
-
-            {inline_box_links}
-
-          </div>
-        </div>
-      </figcaption>
-    </figure>
-    {inline_boxes}
-  </div>
+<div class="4u 12u(mobile)">
+  <section class="highlight">
+    <a href="{link}" target="_blank" class="image featured"><img src="{image}" alt="{alt}" /></a>
+    <h3><a href="{link}" target="_blank">{title}</a></h3>
+    <p>{description}</p>
+  </section>
 </div>'''
 
 def render_portfolio_items(portfolios):
   ret = ''
   for portfolio in portfolios:
-    (inline_boxes, inline_box_links) = render_inline_boxes(portfolios, portfolio)
+
+    try:
+      image = portfolio['thumbnail']['url']
+    except:
+      image = PELICAN.settings['SITEURL'] + '/images/pic{:02}.jpg'.format(random.randrange(2,8))
 
     ret += PORTFOLIO_ITEM.format(
-      size=portfolio['size'],
-      category=' '.join(portfolio['category']).lower(),
-      thumbnail_src=portfolio['thumbnail']['url'],
-      thumbnail_alt=portfolio['thumbnail']['alt'],
+      link=portfolio['link'],
+      image=image,
+      alt='{} thumbnail'.format(portfolio['title']),
       title=portfolio['title'],
-      category_display=' '.join(portfolio['category']),
-      button_url=portfolio['link'],
-      portfolio_number=portfolios.index(portfolio),
-      inline_box_links=inline_box_links,
-      inline_boxes=inline_boxes,
-      date=portfolio['date']
+      date=portfolio['date'],
+      description=portfolio['content']
     )
   return ret
 
 
+PORTFOLIO_TEMPLATE = '''
+<div class="row 150%">
+  {portfolio_items}
+</div>'''
+
+
 def render_portfolio(portfolios):
-  categories = render_categories(portfolios)
   portfolio_items = render_portfolio_items(portfolios)
-  return PORTFOLIO_TEMPLATE.format(navigation=categories, grid=portfolio_items)
+  return PORTFOLIO_TEMPLATE.format(portfolio_items=portfolio_items)
 
 
 def generate_main_portfolios(pelican, portfolios):
   path = pelican.settings['OUTPUT_PATH'] + '/index.html'
   soup = BeautifulSoup(open(path), 'html.parser')
 
-  for portfolio_div in soup.find_all('div', id='portfolio'):
+  for portfolio_div in soup.find_all('div', id='highlights'):
       portfolio_div.string = render_portfolio(portfolios)
 
   with open(path, 'w') as f:
     f.write(soup.prettify(formatter=None))
-
-
-def generate_ajax_portfolios(pelican, portfolios):
-  path = pelican.settings['OUTPUT_PATH'] + '/ajax/portfolio.html'
-
-  content = render_portfolio_items(portfolios)
-
-  with open(path, 'w') as f:
-    f.write(content)
 
 
 def generate_portfolio(pelican):
@@ -203,7 +81,6 @@ def generate_portfolio(pelican):
   PELICAN = pelican
   portfolios = load_portfolios()
   generate_main_portfolios(pelican, portfolios)
-  # generate_ajax_portfolios(pelican, portfolios[4:])
 
 
 def register():
