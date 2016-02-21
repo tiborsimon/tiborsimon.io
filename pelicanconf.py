a6f28028429c018c2d15f46bc04d381817545b84
Tibor Simon <tibor@tiborsimon.io>
2016-02-21 18:46:53 +0100

    Donation, series added

--- pelicanconf.py
+++ pelicanconf.py
@@ -66,7 +66,7 @@ OPEN_GRAPH_FB_APP_ID = 551628881652865
 TAG_CLOUD_SORTING = 'alphabetically'
 
 PLUGIN_PATHS = ['plugins']
-PLUGINS = ['tag_cloud', 'neighbors', 'series', 'summary', 'json-search-system', 'portfolio']
+PLUGINS = ['tag_cloud', 'neighbors', 'series', 'summary', 'portfolio']
 #PLUGINS = ['tspr', 'summary', 'tag_cloud', 'series', 'figure-generator', 'json-search-system', 'bootstrapify']
 #PLUGINS = ['tspr', 'summary', 'json-search-system', 'bootstrapify']
 
