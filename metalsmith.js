203bd364f06d0a26c07e1f4fd554d2f379218187
Tibor Simon <tibor@tiborsimon.io>
2017-02-27 23:44:35 +0100

    Small style update.

--- metalsmith.js
+++ metalsmith.js
@@ -142,7 +142,7 @@ metalsmith(__dirname)
   }))
   .use(pagination({
     'collections.articles': {
-      perPage: 3,
+      perPage: 4,
       first: 'index.html',
       path: 'page/:num/index.html',
       layout: 'index.html'
